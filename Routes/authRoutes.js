88f2769af1ea53af7da97e99566e47b0ba2c62c8
Baroon Shrestha <baroonshrestha@Baroons-MacBook-Air.local>
2025-07-25 14:02:49 +0545

    Updated port and cors

--- Routes/authRoutes.js
+++ Routes/authRoutes.js
@@ -5,11 +5,12 @@ const {
   signOut,
   adminMe,
 } = require("../Controllers/adminLogin");
+const { verifyAdmin } = require("../MIddlewares/verifyAdmin");
 
 const router = express.Router();
 
 router.post("/login", adminLogin);
 router.post("/logout", signOut);
-router.get("/me", adminMe);
+router.get("/me", verifyAdmin, adminMe);
 
 module.exports = router;
